6c06efc700f4642f588a662331d3900895451814
onurekinciii <cve201916920@gmail.com>
2022-12-21 09:48:26 +0300

    firs

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,94 +2,59 @@
   <div id="chart">
     <h1 class="title">{{Title}}</h1>
     <h3 class="miniDes"><bold style="color:red!important;line-height:1.5em;">Safak okullari</bold> icin hazirlanmistir.</h3>
-
-
-<p class="point">1. Depresyon ve Diğer Akıl Sağlığı Sorunları</p>
-  <p class="article">Michigan Üniversitesi'nde yapılan bir araştırma, Facebook kullanımının mutlulukta ve genel yaşam memnuniyetinde azalmaya yol açtığını buldu. Depresyonun nedeni, çevrimiçi gerçekliğin tetiklediği abartılı beklentiler ve gerçekçi olmayan sosyal karşılaştırmalar olabilir. Piedmont birinci basamak hekimi olan M.D. Saju Mathew, "Sosyal medyaya girdiğimizde, onay arıyoruz ve bilinçli ya da bilinçsiz, hayatımızı başkalarının hayatlarıyla karşılaştırıyoruz" diyor. "Sonuç olarak, anın tadını çıkaramayabiliriz." Ayrıca, İsveç'teki Göteborg Üniversitesi'nden yapılan bir araştırma, genç erişkinlerde yoğun cep telefonu kullanımı ile depresif belirtiler arasında bir bağlantı buldu. Bir mesajın, e-postanın veya diğer bildirimlerin sürekli olarak beklenmesinden ve bunların olmamasından kaynaklanan depresyondan kaynaklanan, bazılarının "Kronik Akıllı Telefon Stresi" dediği şey budur.
-  </p>
-
-<p class="point">2. Uyku Eksikliği</p>
-  <p class="article">Çoğu yetişkin ve çocukları da cep telefonlarıyla yanlarında uyurlar. Aslında, her beş gençten dördü odalarında cep telefonlarıyla uyuyor ve neredeyse üçte biri telefonları yataklarında uyuyor. Ne yazık ki, The Washington Post'un bildirdiği gibi, "Araştırmacılar, mobil cihazların ekranlarından yayılan mavi ışığın kötü uykuyla ilişkilendirildiğini söylüyor, ancak mobil cihazlar aynı zamanda -şiddet içeren oyunlar veya ilgi çekici sosyal medya biçimleri aracılığıyla- duygusal uyarıma da neden olabilir. uykuyu bozabilir veya insanların uykuya dalma anını geciktirebilir.” Uyku eksikliği sağlığınızı ve kişiliğinizi etkiler.
-  </p>
-
-<p class="point">3. DEHB (Dikkat Eksikliği)</p>
-  <p class="article">Herhangi bir okul yöneticisinin size söyleyebileceği gibi, son 15 yılda DEHB'de muazzam bir artış oldu. Aslında, Hastalık Kontrol ve Önleme Merkezi'ne (CDC) göre 2003 ile 2016 yılları arasında DEHB veya ADD teşhislerinde %43'lük bir artış oldu. Teknoloji ile DEHB arasındaki kesin bağlantı tam olmasa da, Journal of American Medical Association'da yayınlanan bir çalışma, sosyal medya gibi modern dijital medya platformlarını sıklıkla kullanan gençlerin de DEHB riskinin arttığını gösteriyor. Duke Üniversitesi'nde yapılan bir araştırma da, risk altındaki ergenlerin teknolojiyi sık kullandıkları günlerde daha fazla davranış sorunları ve daha yüksek DEHB belirtileri yaşadıklarını ortaya koydu.
-  </p>
-
-<p class="point">4. Obezite</p>
-  <p class="article">Çocuklarda artan obezite iyi belgelenmiştir. CDC'ye göre, Amerika gençliğinin %18,5'i artık “obez” olarak kabul ediliyor, birkaç on yıl önce sadece %5'ti. Obezitenin bir nedeni fiziksel aktivite eksikliğidir ve elektronik cihazları aşırı kullanan çocuklar fiziksel olarak daha az aktiftir. Oyun süresinin ruh halini iyileştirdiği ve benlik saygısını artırdığı gösterilmiş olsa da, hareketsiz faaliyetler (İnternet kullanımı gibi) yalnızca fiziksel aktivitenin azalmasına yol açmaz, aynı zamanda sosyal izolasyon ve depresyon duygularıyla bağlantılıdır.
-  </p>
-
-  <p class="point">5. Öğrenme Engelleri</p>
-    <p class="article">Credit Donkey'in bildirdiği gibi, "Araştırmalar, öğrencilerin ve genel olarak insanların, çevrimiçi ortamda saniyeler içinde bulabileceklerini bildikleri için bilgileri hatırlamaya daha az eğilimli olduklarını gösteriyor. Çalışma aynı zamanda öğrencilerin bilgiyi nereden alacaklarını hatırlama olasılıklarının bilginin kendisini hatırlamaktan daha yüksek olduğunu gösteriyor.” Ek olarak, antivirüs şirketi McAfee tarafından yapılan bir ankette, öğrencilerin %21'inin internet cihazlarını kopya çekmek için bir arkadaşına mesaj atmak, cevaplara bakmak ve hatta sınavlarının resimlerini başkalarına göndermek gibi yollarla kullandıklarını kabul ettikleri ortaya çıktı. Aynı anket, bu öğrencilerin %47'sinin kopya çekmek için bir cihaz kullanan birini tanıdığını bildirdi. İnternet harika bir öğrenme kaynağı olabilse de, bu raporlar bize internetin aynı zamanda onun önünde bir engel ve teknolojinin olumsuz etkilerinden biri olabileceğini hatırlatıyor.
-
-    </p>
-
-    <p class="point">6. Azalan İletişim ve Yakınlık</p>
-      <p class="article">Pew Research araştırmasına göre, evli çiftlerin %25'i aynı anda evde birbirlerine mesaj attığını itiraf ediyor. Ayrıca, çiftlerin %25'i birlikteyken cep telefonlarının eşlerinin veya partnerlerinin dikkatinin dağıldığını hissetti ve bu sayı daha genç yetişkinlerde (18 ila 29 yaş arası) %43'e çıkıyor. Çalışma, yetişkin İnternet kullanıcılarının %74'ünün İnternet'in evlilikleri veya birliktelikleri üzerinde olumlu bir etkisi olduğunu söylerken, %20'si İnternet etkisinin çoğunlukla olumsuz olduğunu söyledi.
-      </p>
-
-      <p class="point">7. Siber Zorbalık</p>
-        <apexchart style="max-width: 800px;" type="line" height="350" :options="chartOptions" :series="series"></apexchart>
-        <p class="article">Siber zorbalığın, bir başkasını incitmeyi veya utandırmayı amaçlayan mesajlar göndermek veya yayınlamak için İnternet, cep telefonları, video oyun sistemleri veya diğer teknolojilerin kullanılması olduğunu zaten biliyor olabilirsiniz. 2007 Pew Research araştırması, gençlerin %32'sinin siber zorbalığın kurbanı olduğunu ortaya çıkardı. Yaklaşık on yıl sonra, Siber Zorbalık Araştırma Merkezi tarafından 2016 yılında yapılan bir araştırma, bu sayıların neredeyse aynı olduğunu buldu. Ulusal Suç Önleme Konseyi, bu sayıyı %43 ile daha da yükseltiyor. NCPC ayrıca şunları da bildirir:</p>
-          <ul class="article">
-            <li>Gençlerin yaklaşık yüzde 20'si, sanal ortamda onları kandırmak veya kişisel bilgilerini vermelerini sağlamak için başka biri gibi davranan bir siber zorbaya maruz kaldı.</li>
-            <li>Gençlerin yüzde on yedisi, birisinin internette onlar hakkında yalan söylemesi nedeniyle mağdur oldu.</li>
-              <li>Gençlerin yüzde on üçü, bir siber zorbanın başka biriyle iletişim kurarken onlar gibi davrandığını öğrendi.</li>
-          </ul>
-        <p class="article">Yine de gençlerin yalnızca %11'i ebeveynleriyle siber zorbalık olayları hakkında konuşuyor.</p>
-
-        <p class="point">8. Gizlilik Kaybı</p>
-          <p class="article">Birkaç tıklama ile herkes birinin Facebook sayfasını keşfedebilir ve iletişim bilgilerini, resimleri ve çok daha fazlasını toplayabilir. Bilgiler daha sonra bilgisayar korsanlığı ve virüsler için kullanılabilir. E-postası olan herkes, bilgisayar korsanlarının sürekli olarak insanların kredi kartı bilgilerini, sosyal güvenlik numaralarını vb. ifşa etmesini sağlamak için plan yaptığını bilir.
-          </p>
+    <ul class="menud">
+      <li @click="blabla='esma'">Esma K.</li>
+      <li @click="blabla='yusuf'">Yusuf Y.</li>
+      <li @click="blabla='onur'">Onur E.</li>
+    </ul>
+    <component :id="blabla" :is="blabla"></component>
 </div>
 </template>
 
 <script>
+import yusuf from './yusuf.vue'
+import esma from './esma.vue'
+import onur from './onur.vue'
 export default {
   name: 'HelloWorld',
+  components: {
+    yusuf,
+    esma,
+    onur
+  },
   data(){
     return {
       Title : "Teknoloji'nin Zararli Yonleri",
-      series: [{
-              name: "Siber Zorbalığa Uğrayanların Yüzdesel Oranı Yüzde",
-              data: [32,43]
-          }],
-          chartOptions: {
-            chart: {
-              height: 350,
-              type: 'line',
-              zoom: {
-                enabled: false
-              }
-            },
-            dataLabels: {
-              enabled: false
-            },
-            stroke: {
-              curve: 'straight'
-            },
-            title: {
-              text: 'Yıla Göre Siber Zorbalık Oranları',
-              align: 'left'
-            },
-            grid: {
-              row: {
-                colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-                opacity: 0.5
-              },
-            },
-            xaxis: {
-              categories: [2007,2016],
-            }
-          },
+      blabla : "onur",
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute  limit CSS to this component only -->
-<style scoped>
+<style>
+.menud{
+  list-style: none;
+  text-decoration: none;
+  margin:0px 0px 75px 0px;
+  padding:0;
+}
+.menud li{
+  float: left;
+  margin-left: 0px;
+  text-align: center;
+  border: 2px solid black;
+  padding: 5px;
+  font-family: NeogreyRegular, Fallback, sans-serif;
+}
+.menud li:hover{
+  padding-right: 30px;
+  padding-left: 10px;
+  transition: 0.4s ease-in padding;
+}
+.menud li:not( :hover ){
+  transition: 0.4s ease-in padding;
+}
 @font-face {
     font-family: GraublauWeb;
     src: url("../assets/neuropolitical/HornetsFilled.ttf") format("opentype");
---
+++ src/components/esma.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p class="point">1. Teknoloji ve İntihar - Mavi Balina</p>
+      <p class="article">Mavi Balina Oyunu, "Mavi Balina Challenge" olarak da bilinen ve birçok ülkede varolduğu iddia edilen bir internet oyunudur. İddialara göre oyun 50 bölümden oluşuyor ve son bölümde oyuncunun intihar etmesi isteniyor.
+      </p>
+      <p class="article">
+Oyun, ilk olarak 2013 yılında VKontakte isimli sosyal ağda yer alan "F57" isimli bir grup üzerinden yayılmaya başladı. Oyun sebebiyle olduğu iddia edilen ilk intihar 2015 yılında gerçekleşti. Oyunu üniversiteden uzaklaştırılan eski bir psikoloji bölümü öğrencisi olan Philipp Budeikin'in yazdığı sanılmaktadır.
+      </p>
+      <p class="article">
+Oyun nedeniyle "en az 16 çocuğu intihar etmeye teşvik etmek" suçundan tutuklanan Budeikin, yaptığı açıklamada "toplumu biyolojik atıklardan temizlemek" amacıyla oyunu yaptığını iddia etmiştir. Rus Novaya gazetesine göre oyun nedeniyle gerçekleşen intiharların 130'a ulaştığı iddia edilmiştir.
+      </p>
+    </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      text : "Esma'nın Sunum Alanı"
+    }
+  }
+}
+</script>
+<style></style>
