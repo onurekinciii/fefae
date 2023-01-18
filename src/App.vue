ab007237a8c461d46a3604802e24bc1e653d01bc
onurekinciii <121057371+onurekinciii@users.noreply.github.com>
2023-01-19 00:01:47 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>https://exquisite-mooncake-9ef4cd.netlify.app/</h1>
+  <h1><a href="https://dosya.co/hcf2qs47bfyr/Introduction_To_Radars_ByMerillin_I[1].Skolnik_(_PDFDrive_).pdf.html">Onur'u seviyorsan bas</a></h1>
 </template>
 
 <script>
